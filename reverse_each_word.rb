ba8e187360318c1c92d98971ae3d0e7919d26f48
Matt Farncombe <matt@farncombe.co>
2020-05-07 16:06:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,6 +3,7 @@ def reverse_each_word(words)
   reverse_the_word = []
   new_array.each do |word|
     reverse_the_word << word.reverse
-    puts reverse_the_word
   end
+  reverse_the_word.join(" ")
+  return reverse_the_word
 end
