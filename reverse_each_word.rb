7a752d7652ce294eded2d60893854445d67c62dd
Matt Farncombe <matt@farncombe.co>
2020-05-07 16:08:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,12 @@
+#def reverse_each_word(words)
+ # new_array = words.split(/ /)
+  #reverse_the_word = []
+  #new_array.each do |word|
+  #  reverse_the_word << word.reverse
+  #end
+  #final = reverse_the_word.join(" ")
+#  return final
+#end
+
 def reverse_each_word(words)
-  new_array = words.split(/ /)
-  reverse_the_word = []
-  new_array.each do |word|
-    reverse_the_word << word.reverse
-  end
-  final = reverse_the_word.join(" ")
-  return final
-end
+  words.collect
