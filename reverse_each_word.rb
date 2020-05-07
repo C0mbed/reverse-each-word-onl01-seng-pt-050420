1a2c68a5a4649a5779c8a8b4bb8a95c74275eac5
Matt Farncombe <matt@farncombe.co>
2020-05-07 16:02:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,8 @@
 def reverse_each_word(words)
-  
+  new_array = words.split(/ /)
+  reverse_the_word = []
+  new_array.each do |word|
+    reverse_the_word = word.reverse
+  end
+  reverse_the_word.join(" ")
 end
